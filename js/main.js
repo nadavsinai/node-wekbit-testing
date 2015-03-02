a72ac55beb164471720e9bb7f1fad68afc41441a
nadavsinai <ns@nadavsinai.com>
2015-03-02 22:03:35 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+var file = require('./js/file.js');
+var gui = require('nw.gui');
+gui.Window.get().menu = require('.js/menu.js');
+
+
+function clickInput(id) {
+    var event = document.createEvent('MouseEvents');
+    event.initMouseEvent('click');
+    document.getElementById(id).dispatchEvent(event);
+}
+
+document.addEventListener('keyup', function (e) {
+    if (e.keyCode == 'O'.charCodeAt(0) && e.ctrlKey) {
+        clickInput('open');
+    } else if (e.keyCode == 'S'.charCodeAt(0) && e.ctrlKey) {
+        clickInput('save');
+    } else if (e.keyCode == 'N'.charCodeAt(0) && e.ctrlKey) {
+        gui.Window.open('index.html');
+    }
+});
+
+document.getElementById('open').addEventListener('change', function (e) {
+    file.open(this.value, document);
+});
+
+document.getElementById('save').addEventListener('change', function (e) {
+    file.save(this.value, document);
+});
