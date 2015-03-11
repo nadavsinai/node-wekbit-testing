e138b985600f154eb7866335c3c7d53f4643c7c1
Nadav Sinai <ns@nadavsinai.com>
2015-03-12 01:39:30 +0200

    fix e2e tests for 0.12

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,19 +1,37 @@
-var baseFile = __dirname +'/index.html';
+var baseFile = __dirname + '/index.html';
+var nwBinary;
+
+switch (process.platform) {
+    case 'darwin':
+        nwBinary = process.env.PWD + '/node_modules/nw/nwjs/nwjs.app/Contents/MacOS/nwjs';
+        break;
+    case 'linux':
+        nwBinary = process.env.PWD + '/node_modules/nw/bin/nw';
+        break;
+    case 'win32':
+        nwBinary = nwBinary = process.env.PWD + '/node_modules/nw/nwjs/nw.exe';
+        break;
+
+}
 exports.config = {
-    directConnect:true,
+    directConnect: true,
     chromeDriver: './support/chromedriver',
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            // Path to NW.js binary goes here!
+            binary: nwBinary
+        }
+    },
     // Spec patterns are relative to the location of this config.
-    onPrepare:function(){
+    onPrepare: function () {
         browser.ignoreSynchronization = true;
         browser.resetUrl = 'file://' + baseFile;
-        browser.driver.get('file://'+baseFile);
+        browser.driver.get('file://' + baseFile);
     },
-    onComplete:function(){
-        browser.executeScript(function(){
-            quit();
-        });
+    onComplete: function () {
     },
-    baseUrl:__dirname +'/index.html',
+    baseUrl: __dirname + '/index.html',
     specs: [
         './tests/e2e/*.spec.js'
     ]
